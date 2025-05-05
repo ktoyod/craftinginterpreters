c9e394f5e1806a84e728756c37227f4098536723
Kohei Toyoda <ytdh7834@gmail.com>
2025-05-05 19:48:26 +0900

    Implement chapter16 (#28)

--- clox/vm.h
+++ clox/vm.h
@@ -15,13 +15,13 @@ typedef struct {
 
 typedef enum {
         INTERPRET_OK,
-        INTERPRET_COMPLITE_ERROR,
-        INTERPRETE_RUNTIME_ERROR,
+        INTERPRET_COMPILE_ERROR,
+        INTERPRET_RUNTIME_ERROR,
 } InterpretResult;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
 
